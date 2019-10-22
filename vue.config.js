218b3ea74044de4ea216ddd276e7a255d3d9b93c
Toyuye <xiaoping.tao@molbase.com>
2019-10-22 15:08:26 +0800

    vue=> config

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,58 @@ function resolve(dir) {
 module.exports = {
   devServer: {
     port: 8888,
-    open: true
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    proxy: {
+      "/api": {
+        target: `http://localhost:${8888}/mock-api/v1`,
+        changeOrigin: true, // needed for virtual hosted sites
+        pathRewrite: {
+          "^/api":  ''
+        }
+      }
+    }
+  },
+  chainWebpack(config) {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    // https://webpack.js.org/configuration/devtool/#development
+    config
+      .when(process.env.NODE_ENV === 'development',
+        config => config.devtool('cheap-source-map')
+      )
+    config
+      .when(process.env.NODE_ENV !== 'development',
+        config => {
+          config
+            .optimization.splitChunks({
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'chunk-libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial' // only package third parties that are initially dependent
+                },
+                elementUI: {
+                  name: 'chunk-elementUI', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                },
+                commons: {
+                  name: 'chunk-commons',
+                  test: path.resolve(__dirname, 'src/components'),
+                  minChunks: 3, //  minimum common number
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
+              }
+            })
+          config.optimization.runtimeChunk('single')
+        }
+      )
   }
 };
